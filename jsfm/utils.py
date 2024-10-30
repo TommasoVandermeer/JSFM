e135e745a06351e1c38fedf067cd697d583023b7
TommasoVandermeer <tommasovdm@gmail.com>
2024-10-30 12:58:17 +0100

    adjust default agents mass

--- jsfm/utils.py
+++ jsfm/utils.py
@@ -17,5 +17,5 @@ def get_standard_humans_parameters(n_humans:int):
     outputs:
     - parameters (n_humans, 19) - Standard parameters for the humans in the simulation.
     """
-    single_params = jnp.array([0.3, 80., 1., 0.5, 2000., 2000., 0.08, 0.08, 120., 120., 0.6, 0.6, 120000., 240000., 1., 500., 3., 0.1, 0.])
+    single_params = jnp.array([0.3, 75., 1., 0.5, 2000., 2000., 0.08, 0.08, 120., 120., 0.6, 0.6, 120000., 240000., 1., 500., 3., 0.1, 0.])
     return jnp.tile(single_params, (n_humans, 1))
